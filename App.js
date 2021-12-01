e2e6efef1d02678eb3f63f9784fe349183268410
Abderrahim Bouhriz <abderrahimbouhriz@gmail.com>
2021-12-01 21:56:27 +0100

    camera and recognisation

--- App.js
+++ App.js
@@ -1,21 +1,134 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect, useRef } from 'react';
+import {
+  View,
+  StyleSheet,
+  Dimensions,
+  Pressable,
+  Modal,
+  Text,
+  ActivityIndicator,
+} from 'react-native';
+import { Camera } from 'expo-camera';
+
+import TextRecognition from 'react-native-text-recognition';
 
 export default function App() {
+  const [isProcessing, setIsProcessing] = useState(false);
+  const [presentedShape, setPresentedShape] = useState('');
+  const [hasPermission, setHasPermission] = useState(null);
+  const cameraRef = useRef();
+  const [type, setType] = useState(Camera.Constants.Type.back);
+  const handleImageCapture = async () => {
+    setIsProcessing(true);
+    const imageData = await cameraRef.current.takePictureAsync({
+      base64: true,
+      orientation: 'portrait',
+      fixOrientation: true,
+      quality: 0.5,
+    });
+    console.log(imageData.uri);
+    const result = await TextRecognition.recognize(imageData.uri);
+    console.log(result);
+
+  };
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Modal visible={isProcessing} transparent={true} animationType="slide">
+        <View style={styles.modal}>
+          <View style={styles.modalContent}>
+            <Text>Your current shape is {presentedShape}</Text>
+            {presentedShape === '' && <ActivityIndicator size="large" />}
+            <Pressable
+              style={styles.dismissButton}
+              onPress={() => {
+                setPresentedShape('');
+                setIsProcessing(false);
+              }}
+            >
+              <Text>Stop scaning</Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
+
+      <Camera
+        ref={cameraRef}
+        style={styles.camera}
+        type={Camera.Constants.Type.back}
+        autoFocus={true}
+        whiteBalance={Camera.Constants.WhiteBalance.auto}></Camera>
+      <Pressable
+        onPress={() => handleImageCapture()}
+        style={styles.captureButton}>
+        <Text style={styles.title}>Scan</Text>
+      </Pressable>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    width: '100%',
+    height: '100%',
+  },
+  camera: {
+    width: '100%',
+    height: '100%',
+  },
+  captureButton: {
+    position: 'absolute',
+    left: Dimensions.get('screen').width / 2 - 50,
+    bottom: 40,
+    width: 100,
+    zIndex: 100,
+    height: 50,
+    backgroundColor: '#555555',
+    borderRadius: 5,
+  },
+  title: {
+    textAlign: 'center',
+    color: 'white',
+    fontSize: 20,
+    marginVertical: 8
+  },
+  modal: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  modalContent: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 300,
+    height: 300,
+    borderRadius: 24,
+    backgroundColor: 'gray',
+  },
+  dismissButton: {
+    width: 150,
+    height: 50,
+    marginTop: 60,
+    borderRadius: 24,
+    color: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'red',
   },
 });
